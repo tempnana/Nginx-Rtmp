806fa5858f9b2e6516282991f398ff8a68f3c2fd
tempnana <94152736+tempnana@users.noreply.github.com>
2022-03-03 11:33:28 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,9 +10,10 @@ cd ~
 git clone git://github.com/FRiCKLE/ngx_cache_purge.git
 git clone git://github.com/yaoweibin/ngx_http_substitutions_filter_module
 git clone git://github.com/openresty/headers-more-nginx-module.git
+git clone git://github.com/arut/nginx-rtmp-module.git
 wget https://github.com/yaoweibin/nginx_upstream_check_module/archive/refs/heads/master.zip && unzip master.zip
 #wget https://ftp.pcre.org/pub/pcre/pcre-8.43.tar.gz && tar xzvf pcre-8.43.tar.gz
-wget https://github.com/tempnana/Nginx/raw/main/pcre-8.45.tar.gz && tar xzvf pcre-8.45.tar.gz
+wget https://github.com/tempnana/Nginx-Rtmp/raw/main/pcre-8.45.tar.gz && tar xzvf pcre-8.45.tar.gz
 wget https://www.zlib.net/zlib-1.2.11.tar.gz && tar xzvf zlib-1.2.11.tar.gz
 wget https://www.openssl.org/source/openssl-1.1.1d.tar.gz && tar xzvf openssl-1.1.1d.tar.gz
 wget http://nginx.org/download/nginx-1.20.1.tar.gz && tar zxvf nginx-1.20.1.tar.gz
@@ -24,7 +25,7 @@ mkdir /etc/nginx/html -p
 cd nginx-1.20.1
 #make install
 patch -p1 <  ../nginx_upstream_check_module-master/check_1.20.1+.patch
-./configure --prefix=/etc/nginx --sbin-path=/usr/sbin/nginx --modules-path=/usr/lib/nginx/modules --conf-path=/etc/nginx/nginx.conf --error-log-path=/var/log/nginx/error.log --http-log-path=/var/log/nginx/access.log --pid-path=/var/run/nginx.pid --lock-path=/var/run/nginx.lock --http-client-body-temp-path=/var/cache/nginx/client_temp --http-proxy-temp-path=/var/cache/nginx/proxy_temp --http-fastcgi-temp-path=/var/cache/nginx/fastcgi_temp --http-uwsgi-temp-path=/var/cache/nginx/uwsgi_temp --http-scgi-temp-path=/var/cache/nginx/scgi_temp --user=nginx --group=nginx --with-compat --with-file-aio --with-threads --with-http_addition_module --with-http_auth_request_module --with-http_dav_module --with-http_flv_module --with-http_gunzip_module --with-http_gzip_static_module --with-http_mp4_module --with-http_random_index_module --with-http_realip_module --with-http_secure_link_module --with-http_slice_module --with-http_ssl_module --with-http_stub_status_module --with-http_sub_module --with-http_v2_module --with-mail --with-mail_ssl_module --with-stream --with-stream_realip_module --with-stream_ssl_module --with-stream_ssl_preread_module --with-pcre=../pcre-8.45 --with-pcre-jit --with-zlib=../zlib-1.2.11 --with-openssl=../openssl-1.1.1d --with-openssl-opt=no-nextprotoneg --with-debug --add-module=../ngx_cache_purge --add-module=../ngx_http_substitutions_filter_module --add-module=../nginx_upstream_check_module-master --add-module=../headers-more-nginx-module
+./configure --prefix=/etc/nginx --sbin-path=/usr/sbin/nginx --modules-path=/usr/lib/nginx/modules --conf-path=/etc/nginx/nginx.conf --error-log-path=/var/log/nginx/error.log --http-log-path=/var/log/nginx/access.log --pid-path=/var/run/nginx.pid --lock-path=/var/run/nginx.lock --http-client-body-temp-path=/var/cache/nginx/client_temp --http-proxy-temp-path=/var/cache/nginx/proxy_temp --http-fastcgi-temp-path=/var/cache/nginx/fastcgi_temp --http-uwsgi-temp-path=/var/cache/nginx/uwsgi_temp --http-scgi-temp-path=/var/cache/nginx/scgi_temp --user=nginx --group=nginx --with-compat --with-file-aio --with-threads --with-http_addition_module --with-http_auth_request_module --with-http_dav_module --with-http_flv_module --with-http_gunzip_module --with-http_gzip_static_module --with-http_mp4_module --with-http_random_index_module --with-http_realip_module --with-http_secure_link_module --with-http_slice_module --with-http_ssl_module --with-http_stub_status_module --with-http_sub_module --with-http_v2_module --with-mail --with-mail_ssl_module --with-stream --with-stream_realip_module --with-stream_ssl_module --with-stream_ssl_preread_module --with-pcre=../pcre-8.45 --with-pcre-jit --with-zlib=../zlib-1.2.11 --with-openssl=../openssl-1.1.1d --with-openssl-opt=no-nextprotoneg --with-debug --add-module=../ngx_cache_purge --add-module=../ngx_http_substitutions_filter_module --add-module=../nginx_upstream_check_module-master --add-module=../headers-more-nginx-module --add-module=../nginx-rtmp-module
 sleep 1
 make && make install
 cd
@@ -32,7 +33,7 @@ cd
 ln -s /usr/lib/nginx/modules /etc/nginx/modules
 #add vhost support
 cp /etc/nginx/nginx.conf /etc/nginx/nginx.conf.bak
-wget -q -O nginx.conf https://raw.githubusercontent.com/tempnana/Nginx/main/nginx.conf
+wget -q -O nginx.conf https://raw.githubusercontent.com/tempnana/Nginx-Rtmp/main/nginx.conf
 mv nginx.conf /etc/nginx/
 #add system service
 cat >/lib/systemd/system/nginx.service << EOF
